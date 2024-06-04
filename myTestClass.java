2909132984d0506df9ded301998677604d975dfb
ShantanuBokre2024 <bokre.shanu@gmail.com>
2024-06-04 12:34:31 +0530

    Update myTestClass.java
    This is test commit

--- myTestClass.java
+++ myTestClass.java
@@ -3,5 +3,6 @@ public class myTestClass{
     public static void main(String[] args){
 System.out.println("this is java jenkins program");
 System.out.println(new Date());
+System.out.println("Hello this is test commit");
     }
 }
